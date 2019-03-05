199e7d9a40fd49c3fa45c418ae294558b0e80a1f
hdezpedro <37377371+hdezpedro@users.noreply.github.com>
2019-03-04 23:08:46 -0600

    calculadora
    si quedo

---
+++ app/src/main/java/com/example/pruebadiseolinearlayout/MainActivity.java
@@ -0,0 +1,10 @@
+
+package com.example.pruebadiseolinearlayout;
+
+import android.app.Activity;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    public void addOnClick(View view) {
+    }
+}
